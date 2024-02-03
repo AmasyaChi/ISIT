46123018973227eb1ac295cbba756d35ebaad6c6
AmasyaChi <amaliya.2003rhdjhf@gmail.com>
2024-02-04 03:10:38 +0400

    ruff check

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+print("This is a long line of code that definitely exceeds the maximum line length of 80 characters as per PEP8 guidelines, which is why the linter should flag this...........................................................................................................")
 
 @app.get("/")
 async def root():
